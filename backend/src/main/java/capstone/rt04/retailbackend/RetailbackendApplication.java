2d68d118b07afcbaec6c744412a0f9f5654f6794
shawnrpnus <E0175613@u.nus.edu>
2020-02-07 00:43:57 +0800

    Add unversioned files

---
+++ backend/src/main/java/capstone/rt04/retailbackend/RetailbackendApplication.java
@@ -0,0 +1,13 @@
+package capstone.rt04.retailbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailbackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/capstone/rt04/retailbackend/RetailbackendApplicationTests.java
@@ -0,0 +1,20 @@
+package capstone.rt04.retailbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+class RetailbackendApplicationTests {
+
+
+    @Test
+    void test() {
+
+    }
+
+}
