0ade205b0b82bbe4ff117f4dc9b44b811b8079ac
lolhaha95 <bryan_wang19@hotmail.com>
2020-02-08 03:59:18 +0800

    Did some of warehouse, will continue tmr.

---
+++ backend/src/main/java/capstone/rt04/retailbackend/util/exceptions/warehouse/WarehouseNotFoundException.java
@@ -0,0 +1,13 @@
+package capstone.rt04.retailbackend.util.exceptions.warehouse;
+
+public class WarehouseNotFoundException extends Exception {
+
+
+    public WarehouseNotFoundException() {
+    }
+
+    public WarehouseNotFoundException(String msg) {
+        super(msg);
+    }
+
+}
