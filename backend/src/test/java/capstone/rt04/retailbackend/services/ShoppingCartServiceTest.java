a589b0367032e6ffe5c92a8a1b78cca8d1dcb546
shawnrpnus <E0175613@u.nus.edu>
2020-02-09 16:49:53 +0800

    Shift customer test email to be constant

--- backend/src/test/java/capstone/rt04/retailbackend/services/ShoppingCartServiceTest.java
+++ backend/src/test/java/capstone/rt04/retailbackend/services/ShoppingCartServiceTest.java
@@ -26,64 +26,9 @@ public class ShoppingCartServiceTest extends  CustomerProductCategoryTestSetup {
     @Autowired
     private CustomerService customerService;
 
-//    @Autowired
-//    private CategoryService categoryService;
-//
-//    @Autowired
-//    private ProductService productService;
-//    private static Long categoryId;
-//    private static Long productId;
-//    private static Long productVariantId;
-
-//    @Before
-//    public void beforeEachTest() throws Exception{
-//        Customer expectedValidCustomer = new Customer("Tony", "Stark", "tonystark@gmail.com", "spiderman");
-//        Customer testValidCustomer = customerService.createNewCustomer(expectedValidCustomer);
-//        assertThat(testValidCustomer.getCustomerId()).isNotNull();
-//        assertThat(testValidCustomer).isEqualTo(expectedValidCustomer);
-//        assertThat(testValidCustomer.getOnlineShoppingCart()).isNotNull();
-//        assertThat(testValidCustomer.getInStoreShoppingCart()).isNotNull();
-//
-//        Product validProduct = new Product("Fila Disruptor II", "Fila", BigDecimal.valueOf(89.90), BigDecimal.valueOf(39.90));
-//        Category category = categoryService.createNewCategory(new Category("Shoes"), null);
-//        validProduct.setCategory(category);
-//
-//        Product result = productService.createNewProduct(validProduct, category.getCategoryId(), null);
-//        assertThat(result).isEqualTo(validProduct);
-//        categoryId = category.getCategoryId();
-//        productId = result.getProductId();
-//
-//        Product product = productService.retrieveProductById(productId);
-//        ProductVariant validProductVariant = new ProductVariant("SKU001", "White", null, product, null);
-//
-//        ProductVariant productVariant = productService.createProductVariant(validProductVariant);
-//        productVariantId = productVariant.getProductVariantId();
-//        assertThat(productVariant).isEqualTo(validProductVariant);
-//    }
-//
-//    @After
-//    public void afterEachTest() throws Exception{
-//        Customer validCustomer = customerService.retrieveCustomerByEmail("tonystark@gmail.com");
-//        Customer removedCustomer = customerService.removeCustomer(validCustomer.getCustomerId());
-//        assertThat(removedCustomer.getCustomerId()).isEqualTo(validCustomer.getCustomerId());
-//
-//        Product productToRemove = productService.retrieveProductById(productId);
-//        Product removedProduct = productService.deleteProduct(productToRemove.getProductId()); //deletes prod variant also
-//        assertThat(removedProduct.getProductId()).isEqualTo(productToRemove.getProductId());
-//
-//        Category categoryToRemove = categoryService.retrieveCategoryByCategoryId(categoryId);
-//        Long categoryId = categoryToRemove.getCategoryId();
-//        Category removedCategory = categoryService.deleteCategory(categoryId);
-//        assertThat(removedCategory.getCategoryId()).isEqualTo(categoryId);
-//
-//        productId = null;
-//        categoryId = null;
-//        productVariantId = null;
-//    }
-
     @Test
     public void addUpdateRemoveProductVariantToShoppingCart() throws Exception {
-        Customer validCustomer = customerService.retrieveCustomerByEmail("tonystark@gmail.com");
+        Customer validCustomer = customerService.retrieveCustomerByEmail(VALID_CUST_EMAIL);
 
         //add
         shoppingCartService.updateQuantityOfProductVariant(1, productVariantId, validCustomer.getCustomerId(), ONLINE_SHOPPING_CART);
