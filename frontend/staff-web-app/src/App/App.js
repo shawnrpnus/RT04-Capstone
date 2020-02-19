19e5500fcee52bbb4a1cbb8cfd1c829be1a18d8c
shawnrpnus <E0175613@u.nus.edu>
2020-02-19 22:23:04 +0800

    FIX BUGS WITH COLLAPSE

--- frontend/staff-web-app/src/App/App.js
+++ frontend/staff-web-app/src/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './Router';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../scss/app.scss';
 import ScrollToTop from "./ScrollToTop";
 import store from "./store"
