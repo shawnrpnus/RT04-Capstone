2a32346bd259a93d721680eb0b75d75346a9e30c
LiJinTan <55802941+LiJinTan@users.noreply.github.com>
2020-02-21 20:28:43 +0800

    Review backend added

---
+++ backend/src/main/java/capstone/rt04/retailbackend/util/exceptions/review/ReviewNotFoundException.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capstone.rt04.retailbackend.util.exceptions.review;
+
+/**
+ *
+ *
+ */
+public class ReviewNotFoundException extends Exception {
+
+    public ReviewNotFoundException() {
+    }
+
+    public ReviewNotFoundException(String msg) {
+        super(msg);
+    }
+    
+}
