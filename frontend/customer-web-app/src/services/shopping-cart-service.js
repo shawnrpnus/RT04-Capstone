7859de14f34732724e2243f5ff1ff4d1573bc0ce
shawnrpnus <E0175613@u.nus.edu>
2020-02-24 02:45:15 +0800

    Remove old customer-web-app fix

--- frontend/customer-web-app/src/services/shopping-cart-service.js
+++
@@ -1,7 +0,0 @@
-export const getCartTotal = cartItems => {
-    let total = 0;
-    for(let i=0; i<cartItems.length; i++){
-        total += parseInt(cartItems[i].qty, 10)*parseInt((cartItems[i].price*cartItems[i].discount/100), 10);
-    }
-    return total;
-};
