4cab2aab213a0bd50065abcadf8371a45d945c05
shawnrpnus <E0175613@u.nus.edu>
2020-02-24 05:23:09 +0800

    Add basic templating to customer-web-app

---
+++ frontend/customer-web-app/src/App/Routes/WrappedRoutes/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Layout from "components/Layout";
+
+export default () => (
+  <div>
+    <Layout>
+      <Route />
+    </Layout>
+  </div>
+);
