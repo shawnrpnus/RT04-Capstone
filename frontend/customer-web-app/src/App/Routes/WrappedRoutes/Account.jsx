8415cee09a86072fd59674543001ec7066523543
shawnrpnus <E0175613@u.nus.edu>
2020-02-26 15:39:08 +0800

    Add basic profile page template

--- frontend/customer-web-app/src/App/Routes/WrappedRoutes/Account.jsx
+++ frontend/customer-web-app/src/App/Routes/WrappedRoutes/Account.jsx
@@ -4,11 +4,13 @@ import Register from "components/Register";
 import VerifyEmailChecker from "components/EmailVerification/VerifyEmailChecker";
 import VerifyEmailPrompt from "components/EmailVerification/VerifyEmailPrompt";
 import LoginPage from "components/Login/LoginPage";
+import ProfilePage from "components/Profile/ProfilePage";
 
 export default () => (
   <Switch>
     <Route exact path="/account/login" component={LoginPage} />
     <Route exact path="/account/register" component={Register} />
+    <Route exact path="/account/profile" component={ProfilePage} />
     <Route exact path="/account/verifyEmail" component={VerifyEmailPrompt} />
     <Route
       exact
