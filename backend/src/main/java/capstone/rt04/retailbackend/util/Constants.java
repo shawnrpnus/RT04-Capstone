7913624bffc92f15f27a6f5c31253d75becd8116
shawnrpnus <E0175613@u.nus.edu>
2020-03-17 23:51:39 +0800

    Update production .env for customer-web-app

--- backend/src/main/java/capstone/rt04/retailbackend/util/Constants.java
+++ backend/src/main/java/capstone/rt04/retailbackend/util/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String ONLINE_SHOPPING_CART = "online";
 
     public static final String[] ALLOWED_ORIGINS = {"http://localhost:3000",
-            "http://localhost:3001", "http://192.168.99.100:3000"};
+            "http://localhost:3001", "http://192.168.99.100:3000", "https://apricot-and-nut.herokuapp.com"};
 
     public static final String SECRET_KEY = "THISISSECRET";
 }
