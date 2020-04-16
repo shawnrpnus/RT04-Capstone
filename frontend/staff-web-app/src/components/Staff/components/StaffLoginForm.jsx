328cea7a646c5fd43103d260cba87cf096fdb196
winphong <winphong@comp.nus.edu.sg>
2020-04-17 01:40:56 +0800

    added logo at staff login page

--- frontend/staff-web-app/src/components/Staff/components/StaffLoginForm.jsx
+++ frontend/staff-web-app/src/components/Staff/components/StaffLoginForm.jsx
@@ -18,7 +18,7 @@ class StaffLoginForm extends Component {
   static propTypes = {
     errors: PropTypes.object,
     clearErrors: PropTypes.func,
-    disabled: PropTypes.bool
+    disabled: PropTypes.bool,
   };
 
   constructor(props) {
@@ -27,11 +27,11 @@ class StaffLoginForm extends Component {
       username: "",
       password: "",
       showPassword: false,
-      store: {}
+      store: {},
     };
   }
 
-  onChange = e => {
+  onChange = (e) => {
     const name = e.target.name;
     this.setState({ [name]: e.target.value }); //computed property name syntax
     if (Object.keys(this.props.errors).length !== 0) {
@@ -39,16 +39,16 @@ class StaffLoginForm extends Component {
     }
   };
 
-  onClick = e => {
+  onClick = (e) => {
     this.props.history.push(`/resetPassword`);
   };
 
   showPassword(e) {
     e.preventDefault();
-    this.setState(prevState => ({ showPassword: !prevState.showPassword }));
+    this.setState((prevState) => ({ showPassword: !prevState.showPassword }));
   }
 
-  handleSubmit = e => {
+  handleSubmit = (e) => {
     e.preventDefault();
     const { username, password, store } = this.state;
     const req = new StaffLoginRequest(username, password);
@@ -69,11 +69,24 @@ class StaffLoginForm extends Component {
           style={{ height: "100vh" }}
         >
           <div className="account__card">
+            <div
+              style={{
+                width: "100%",
+                marginBottom: "10%",
+                textAlign: "center",
+                transform: "scale(1.2)",
+              }}
+            >
+              <img
+                style={{ width: "auto" }}
+                src="https://res.cloudinary.com/rt04capstone/image/upload/v1583433855/rsz_1apricot-nut-logo-word_jzhocy.png"
+              />
+            </div>
+
             <div className="account__head">
               <h3 className="account__title">Welcome back</h3>
               <h4 className="account__subhead subhead">Login to get started</h4>
             </div>
-
             <Grid container spacing={3}>
               <Grid item xs={12} md={12}>
                 <form className="material-form">
@@ -106,7 +119,7 @@ class StaffLoginForm extends Component {
                   <div className="form__form-group">
                     <Button
                       className="account__btn"
-                      onClick={e => this.handleSubmit(e)}
+                      onClick={(e) => this.handleSubmit(e)}
                       color="primary"
                       disabled={hasErrors}
                       style={{ margin: "5% 0" }}
@@ -125,18 +138,18 @@ class StaffLoginForm extends Component {
 }
 
 //mapping global state to this component
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   staffStore: state.storeEntity.selectedStore,
   loggedInStaff: state.staffEntity.loggedInStaff,
   errors: state.errors,
-  allStores: state.storeEntity.allStores
+  allStores: state.storeEntity.allStores,
 });
 
 const mapDispatchToProps = {
   retrieveAllStores,
   staffLogin,
   clearErrors,
-  updateErrors
+  updateErrors,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(StaffLoginForm);
